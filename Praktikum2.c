7195d756721d304ca5b346c8033bf664d2e6c94f
Moritz Sundermann <moritz.sundermann98@gmail.com>
2017-11-06 15:34:37 +0100

    Erste Schritte zum Projekt gestartet: - Array brett erstellt - ausgabe- Funtion einer potentiellen Lösung implementiert

--- Praktikum2.c
+++ Praktikum2.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-int main(int argc, char* argv[]) {
-    printf("Hello World!");
+void ausgabe(int brett[]) {
+    char feld[8][8];
+    //Feld leeren:
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            feld[i][j] = ' ';
+        }
+    }
+    //Feld mit Positionen befüllen:
+    for (int i = 0; i < 8; i++) {
+        feld[brett[i] - 1][i] = 'D';
+    }
+
+    //Feld ausdrucken:
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            printf("|%c", feld[i][j]);
+        }
+        printf("|");
+        printf("\n");
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int brett[8];
+    for (int i = 0; i < 8; i++) {
+        brett[i] = 2;
+    }
+
+    ausgabe(brett);
     return EXIT_SUCCESS;
 }
