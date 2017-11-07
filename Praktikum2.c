03f834cfee9ea01a394837aa71bad00b969ef974
Moritz Sundermann <moritz.sundermann98@gmail.com>
2017-11-07 13:10:34 +0100

    - setze- Funktion, bedroht- Funktion implementiert - noch fehlerhaft im Ergebnis, aber Grundkonzept funktioniert

--- Praktikum2.c
+++ Praktikum2.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
 void ausgabe(int brett[]) {
     char feld[8][8];
-    //Feld leeren:
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++) {                   //Feld leeren:
         for (int j = 0; j < 8; j++) {
             feld[i][j] = ' ';
         }
     }
-    //Feld mit Positionen befüllen:
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++) {                   //Feld mit Positionen befüllen:
         feld[brett[i]][i] = 'D';
     }
-    //Feld ausdrucken:
-    for (int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++) {                   //Feld ausdrucken:
         for (int j = 0; j < 8; j++) {
             printf("|%c", feld[i][j]);
         }
@@ -24,20 +20,38 @@ void ausgabe(int brett[]) {
     }
 }
 
-void setze (int brett[]){
-    brett[0]=0;
-
-
-
+int bedroht(int brett[], int dame, int i){
+    for(int j=0;j<dame;j++){                        //Spaltenweise die Damen absuchen (nur die vor der zu setzenden Dame, deswegen '<Dame') und schauen,
+                                                    //ob die zu setzende Dame von den vorherigen bedroht wird oder nicht
+        if((brett[j]==i) || (abs(brett[j]-i)==abs(j-dame))){
+            return 1;                               //Die Dame wird bedroht
+        }
+    }
+    return 0;                                       //Die Dame wird nicht bedroht
 }
 
-int main(int argc, char *argv[]) {
-    //Schachbrett anlegen(8x8):
-    int brett[8]; //brett[i] = j, in der i-ten Spalte des Schachbrettes, steht die Dame auf der j-ten Reihe
-    for (int i = 0; i < 8; i++) {
+void setze(int brett[], int dame){
+    if(dame != 8){                                  //Wenn noch nicht alle Damen gesetzt wurden:
+        for(int i=0;i<8;i++){                       //Zeilenweise durch das Schachbrett laufen und nach einer Möglichkeit zum setzen der Dame suchen:
+            if(!(bedroht(brett, dame, i))){         //'i' = i-te Zeile, in der bereits eine Dame vor zu setzenden Dame 'dame' stehen kann und diese sich dann gegenseitig schlagen könnten
+                brett[dame]=i;                      //Die n-te Dame ist gleichzeitig auch die n-te Spalte auf dem Spielfeld
+                setze(brett, dame+1);               //Überprüft im Falle eines Treffers die nächste Dame und geht rekursiv vor,
+                                                    //damit die Dame nicht entgültig gesetzt wird, sondern bei einem Fehlschlagen des Algorithmusses, umgesetzt wird
+            }
+        }
+    }
+    else{                                           //Der Fall, wenn alle Damen gesetzt wurden:
+        return;
+    }
+}
+
+int main(int argc, char *argv[]){
+    int brett[8];                                   //Schachbrett anlegen(8x8):
+    for (int i = 0; i < 8; i++){                    //brett[i] = j := in der i-ten Spalte des Schachbrettes, steht die Dame auf der j-ten Reihe
         brett[i] = -9999;
     }
-    setze(brett);
+    int dame = 0;                                   //Am Anfang muss zunächst die 0. (=erste) Dame gesetzt werden
+    setze(brett, dame);
     ausgabe(brett);
     return EXIT_SUCCESS;
 }
