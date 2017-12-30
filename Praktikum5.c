8e1284907425be2dbb95d20280701c778f31156b
Moritz Sundermann <moritz.sundermann98@gmail.com>
2017-12-30 18:59:11 +0100

    - Rabin- Karp Suchverfahren implementiert - durchlaufende Schritte pro Suche ausgeben hinzugefügt

--- Praktikum5.c
+++ Praktikum5.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-#define ALPHABETGROESSE 26
-#define MAX 100
+#define ALPHABETGROESSE 26//fuer mischarsearch
+#define MAX 100           //fuer suchText
 
+#define PRIMZAHL 33554393 //fuer rksearch
+#define BASIS 32          //fuer rksearch
+
+int anzahlSchritte;
+//MISCHARSEARCH (BOYER- MOORE):
 /**
  * Gibt, egal ob Klein- oder Grossbuchstabe, die i. Position des uebergebenen Buchstaben im deutschen Alphabet an.
  * @param c: der uebergebene Buchstabe, von dem man die i. Position wissen möchte
@@ -33,14 +37,13 @@ void initskip(char suchText[], int skip[]){
     for(int i = 0; i < ALPHABETGROESSE; i++) {
         skip[i] = m;
     }
-
     for(int j = 0; j<m-1; j++){
         skip[returnIndex(suchText[j])] = m-j-1;
     }
 }
 
 /**
- * Funktion um eine bestimmte Buchstabenfolge in einem Text zu suchen.
+ * Funktion um eine bestimmte Buchstabenfolge in einem Text zu suchen. Beruht auf Verwendung einer Skip- Tabelle.
  * @param suchText: der zu suchende Text/Zeichenfolge
  * @param text: der Text worin gesucht wird
  * @param skip: die Tabelle(Array), worin steht, wie weit das SuchWort nach rechts verschoben werden kann,
@@ -48,7 +51,7 @@ void initskip(char suchText[], int skip[]){
  * @return: -1: die zu suchende Zeichenkette im Text wurde nicht gefunden
  *           X: die zu suchende Zeichenkette wurde ab X. Stelle im Text gefunden
  */
-int mischarsearch(char suchText[], char text[], int skip[]) {
+int mischarsearch(char text[], char suchText[], int skip[]) {
     int laengeSuche = (int)strlen(suchText);
     int laengeText = (int)strlen(text);
     int i, j, t;
@@ -61,11 +64,44 @@ int mischarsearch(char suchText[], char text[], int skip[]) {
             i += (laengeSuche - j > t) ? laengeSuche - j : t;         //springe um skip(t), mindestens aber um 1 nach rechts oder springe um ein ganzes Wort(Fall, wenn if wahr ist)
             if (i >= laengeText) return -1;                           //der Fall, wenn das Wort nicht gefunden wurde und man am Ende des Textes angelangt ist
             j = laengeSuche - 1;                                      //setze Musterzeiger ganz nach rechts, um eine erneute Suche der Zeichenfolge vorzubereiten
+            anzahlSchritte++;
         }
     }
-    return i+1;
+    return i+1;                                                       //genau die Stelle, ab der das SuchWort im Text gefunden werden kann
 }
 
+//RKSEARCH (RABIN- KARB):
+/**
+ * Funktion um eine bestimmte Buchstabenfolge in einem Text zu suchen. Verfahren funktioniert mit der Berechnung des Hash- Wertes
+ * des SuchWortes und einem genauso langen Abschnitt im Text, der solange nach rechts verschoben wird,
+ * bis sich die Hashwerte gleichen, dann ist Wort gefunden.
+ * @param suchText: der zu suchende Text/Zeichenfolge
+ * @param text: der Text worin gesucht wird
+ * @return: -1: die zu suchende Zeichenkette im Text wurde nicht gefunden
+ *           X: die zu suchende Zeichenkette wurde ab X. Stelle im Text gefunden
+ */
+int rksearch(char text[], char suchText[]) {
+    int dM = 1;
+    int hashSuchText = 0, hashText = 0;
+    int laengeSuche = (int)strlen(suchText);
+    int laengeText = (int)strlen(text);
+
+    int i;
+    for(i = 1; i < laengeSuche; i++) dM = (BASIS*dM) % PRIMZAHL;
+    for(i = 0; i < laengeSuche; i++) {
+        hashSuchText = (hashSuchText*BASIS + returnIndex(suchText[i])) % PRIMZAHL;      //hier wird von den einzelnen Buchstaben aus der Hashwert des gesamten SuchWortes berechnet
+        hashText = (hashText*BASIS + returnIndex(text[i])) % PRIMZAHL;                  //hier wird von den einzelnen Buchstaben aus der Hashwert des gesamten Abschnittes gleicher Laenge des SuchWortes im Text berechnet
+    }
+    for (i = 0; hashSuchText != hashText; i++) {                                        //solange ueber den Text gehen bis der Hash- Wert des Suchwortes mit dem Hashwert des Abschnittes, der die gleiche Laenge wie das Suchwort hat, gleich ist
+        anzahlSchritte ++;
+        hashText = (hashText + BASIS*PRIMZAHL - returnIndex(text[i])*dM) % PRIMZAHL;    //Produkt BASIS*PRIMZAHL zur Sicherstellung, dass hashText > 0
+        hashText = (hashText*BASIS + returnIndex(text[i+laengeSuche])) % PRIMZAHL;      //beide "hashText="- Zeilen sind eine Rechnung mit dem HashText- Wert und tragen dazu bei, dass der Hash- Wert des naechsten Abschnittes(der um ein nach rechts gerutscht ist) berechnet wird
+        if (i > laengeText-laengeSuche) return -1;                                      //wenn der Indes 'i' an dem Punkt im Text angekommen ist, dass das zu suchende Wort gerade nicht mehr rein passt, wird zurückgegeben, dass das Wort nicht gefunden wurde
+    }
+    return i;                                                                           //genau die Stelle, ab der das SuchWort im Text gefunden werden kann
+}
+
+//ERGEBNISSE:
 /**
  * Der Benutzer gibt ein zu suchendes Wort ein. Dieses Wort wird mithilfe des mischarsearch- Algorithmus gesucht.
  * Danach wird eine dazu passende Ausgabe gemacht, ob das Wort gefunden wurde oder nicht.
@@ -73,17 +109,38 @@ int mischarsearch(char suchText[], char text[], int skip[]) {
  * @param suchText: das zu suchende Suchwort
  * @param skip: die Tabelle(Array), worin steht, wie weit das SuchWort nach rechts verschoben werden kann,
  *              wenn es zu einen Unterschied zwischen den Buchstaben im SuchWort und im zu suchenden Text gibt
+ * @return: -1: die zu suchende Zeichenkette im Text wurde nicht gefunden
+ *           X: die zu suchende Zeichenkette wurde ab X. Stelle im Text gefunden
  */
 int ergebnisMischarsearch(char text[], char suchText[], int skip[]) {
-    int ergebnis = mischarsearch(suchText, text, skip);
+    int ergebnis = mischarsearch(text, suchText, skip);
     if(ergebnis == -1){
-        printf("Das Wort wurde nicht gefunden!\n\n");
+        printf("Das Wort wurde nicht gefunden!(durchgeführt mit Boyer- Moore)\n(Benötigte Schritte: %d)\n\n",anzahlSchritte);
     }else{
-        printf("Das Wort wurde ab %d. Stelle im zu suchenden Text gefunden!\n\n", ergebnis+1);
+        printf("Das Wort wurde ab %d. Stelle im zu suchenden Text gefunden!(durchgeführt mit Boyer- Moore)\n(Benötigte Schritte: %d)\n\n", ergebnis+1,anzahlSchritte);
     }
     return ergebnis;
 }
 
+/**
+ * Der Benutzer gibt ein zu suchendes Wort ein. Dieses Wort wird mithilfe des rksearch- Algorithmus gesucht.
+ * Danach wird eine dazu passende Ausgabe gemacht, ob das Wort gefunden wurde oder nicht.
+ * @param text: der konstante Text, in dem gesucht werden soll
+ * @param suchText: das zu suchende Suchwort
+ * @return: -1: die zu suchende Zeichenkette im Text wurde nicht gefunden
+ *           X: die zu suchende Zeichenkette wurde ab X. Stelle im Text gefunden
+ */
+int ergebnisRksearch(char text[], char suchText[]) {
+    int ergebnis = rksearch(text, suchText);
+    if(ergebnis == -1){
+        printf("Das Wort wurde nicht gefunden!(durchgeführt mit Rabin- Karb)\n(Benötigte Schritte: %d)\n\n",anzahlSchritte);
+    }else{
+        printf("Das Wort wurde ab %d. Stelle im zu suchenden Text gefunden!(durchgeführt mit Rabin- Karb)\n(Benötigte Schritte: %d)\n\n", ergebnis+1,anzahlSchritte);
+    }
+    return ergebnis;
+}
+
+//AUSGABE DES ERGEBNISSES IM TEXT:
 /**
  * Gibt den gesamten Text mit markiertem Suchtwort aus.
  * @param text: der konstante Text, in dem gesucht werden soll
@@ -111,22 +168,27 @@ void ergebnisAusgabe(char text[], char suchText[], int ergebnis) {
  * @return: Abbruchbeding
  */
     int main(int argc, char *argv[]) {
-        char text[] = "Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte";
-        int skip[ALPHABETGROESSE]; //Buchstaben von 1-26; 0. Stelle fuer Leerzeichen reserviert
+        char text[] = "Weit hinten, hinter den Wortbergen, fern der Laender Vokalien und Konsonantien leben die Blindtexte.";
+        int skip[ALPHABETGROESSE]; //Buchstaben von 1-26; 0. Stelle fuer Leerzeichen reserviert //fuer mischarsearch
         char suchText[MAX];
 
         printf("Menü\nGeben Sie an, was Sie machen wollen:\n\n");
-        int eingabe = 1;
-        int ergebnis = -1;
+        int eingabe = 0;
+        int ergebnis = 0;
         while(eingabe != 5){
             printf("1) konstanten Text ausgeben\n2) Suchwort eingeben und prüfen[mit Boyer- Moore]\n3) Suchwort eingeben und prüfen[mit Rabin- Karp]\n4) Suchwort im Text anzeigen lassen\n5) Programm beenden\n\n");
             scanf("%d",&eingabe);
+            //ergebnis = 0;
+            anzahlSchritte = 0;
             switch(eingabe){
                 case 1: printf("%s\n\n",text); break;
                 case 2: printf("Geben sie ein zu suchendes Wort ein:\n");
                         scanf("%s",suchText);
                         ergebnis = ergebnisMischarsearch(text, suchText, skip); break;
-                case 4: ergebnisAusgabe(text, suchText, ergebnis);
+                case 3: printf("Geben sie ein zu suchendes Wort ein:\n");
+                        scanf("%s",suchText);
+                        ergebnis = ergebnisRksearch(text, suchText); break;
+                case 4: ergebnisAusgabe(text, suchText, ergebnis); break;
                 default: break;
             }
         }
