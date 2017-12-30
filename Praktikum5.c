b97a4fbd523f8e7e529540422fa81bcfe267ce85
Moritz Sundermann <moritz.sundermann98@gmail.com>
2017-12-30 15:21:59 +0100

    - Menü hinzugefügt - Benutzereingaben für zu suchendes Wort hinzugefügt - Möglichkeit Suchwort im Text anzeigen zu lassen

--- Praktikum5.c
+++ Praktikum5.c
@@ -3,11 +3,12 @@
 #include <string.h>
 
 #define ALPHABETGROESSE 26
+#define MAX 100
 
 /**
  * Gibt, egal ob Klein- oder Grossbuchstabe, die i. Position des uebergebenen Buchstaben im deutschen Alphabet an.
- * @param c: Der uebergebene Buchstabe, von dem man die i. Position wissen möchte
- * @return: 0: Wenn 'c' ein Leerzeichen ist, 1-26: ein i. Buchstabe im Alphabet, -1: kein Leerzeichen oder Buchstabe
+ * @param c: der uebergebene Buchstabe, von dem man die i. Position wissen möchte
+ * @return: 0: wenn 'c' ein Leerzeichen ist, 1-26: ein i. Buchstabe im Alphabet, -1: kein Leerzeichen oder Buchstabe
  */
 int returnIndex(char c) {
     if (c == ' ') {
@@ -23,8 +24,8 @@ int returnIndex(char c) {
  * Befüllt ein skip- Array, das als eine Art Tabelle dient, um nachher beim Suchen des SuchWortes zu schauen,
  * wie weit man das zu suchende Wort(je nach aktuellen Buchstabe des Textes) nach rechts verschieben darf,
  * wenn die zu vergleichenden Buchstaben(einmal vom SuchWort und einmal vom Text) unterschiedlich sind.
- * @param suchText: Der zu suchende suchText im Text
- * @param skip: Das als Tabelle dienende skip- Array
+ * @param suchText: der zu suchende suchText im Text
+ * @param skip: das als Tabelle dienende skip- Array
  */
 void initskip(char suchText[], int skip[]){
     int m = (int)strlen(suchText);
@@ -40,11 +41,12 @@ void initskip(char suchText[], int skip[]){
 
 /**
  * Funktion um eine bestimmte Buchstabenfolge in einem Text zu suchen.
- * @param suchText: Der zu suchende Text/Zeichenfolge
- * @param text: Der Text worin gesucht wird
- * @param skip: Die Tabelle(Array), worin steht, wie weit das SuchWort nach rechts verschoben werden kann,
+ * @param suchText: der zu suchende Text/Zeichenfolge
+ * @param text: der Text worin gesucht wird
+ * @param skip: die Tabelle(Array), worin steht, wie weit das SuchWort nach rechts verschoben werden kann,
  *              wenn es zu einen Unterschied zwischen den Buchstaben im SuchWort und im zu suchenden Text gibt
- * @return
+ * @return: -1: die zu suchende Zeichenkette im Text wurde nicht gefunden
+ *           X: die zu suchende Zeichenkette wurde ab X. Stelle im Text gefunden
  */
 int mischarsearch(char suchText[], char text[], int skip[]) {
     int laengeSuche = (int)strlen(suchText);
@@ -61,7 +63,46 @@ int mischarsearch(char suchText[], char text[], int skip[]) {
             j = laengeSuche - 1;                                      //setze Musterzeiger ganz nach rechts, um eine erneute Suche der Zeichenfolge vorzubereiten
         }
     }
-    return i+2;
+    return i+1;
+}
+
+/**
+ * Der Benutzer gibt ein zu suchendes Wort ein. Dieses Wort wird mithilfe des mischarsearch- Algorithmus gesucht.
+ * Danach wird eine dazu passende Ausgabe gemacht, ob das Wort gefunden wurde oder nicht.
+ * @param text: der konstante Text, in dem gesucht werden soll
+ * @param suchText: das zu suchende Suchwort
+ * @param skip: die Tabelle(Array), worin steht, wie weit das SuchWort nach rechts verschoben werden kann,
+ *              wenn es zu einen Unterschied zwischen den Buchstaben im SuchWort und im zu suchenden Text gibt
+ */
+int ergebnisMischarsearch(char text[], char suchText[], int skip[]) {
+    int ergebnis = mischarsearch(suchText, text, skip);
+    if(ergebnis == -1){
+        printf("Das Wort wurde nicht gefunden!\n\n");
+    }else{
+        printf("Das Wort wurde ab %d. Stelle im zu suchenden Text gefunden!\n\n", ergebnis+1);
+    }
+    return ergebnis;
+}
+
+/**
+ * Gibt den gesamten Text mit markiertem Suchtwort aus.
+ * @param text: der konstante Text, in dem gesucht werden soll
+ * @param suchText: das zu suchende Suchwort
+ * @param ergebnis: die Stelle, ab der das Suchwort gefunden wurde, bei Wert '-1' wurde das Wort nicht gefunden
+ */
+void ergebnisAusgabe(char text[], char suchText[], int ergebnis) {
+    if(ergebnis == -1){
+        printf("Das Suchwort konnte nicht gefunden und somit nicht im Text angezeigt werden!\n\n");
+        return;
+    }
+    for (int i = 0; i < ergebnis ; ++i) {
+        printf("%c", text[i]);
+    }
+    printf("--->%s<---",suchText);
+    for (int j = ergebnis+(int)strlen(suchText); j < (int)strlen(text); j++) {
+        printf("%c", text[j]);
+    }
+    printf("\n\n");
 }
 
 /**
@@ -70,15 +111,24 @@ int mischarsearch(char suchText[], char text[], int skip[]) {
  * @return: Abbruchbeding
  */
     int main(int argc, char *argv[]) {
-        char suchText[] = "Weit hintene";
         char text[] = "Weit hinten, hinter den Wortbergen, fern der Länder Vokalien und Konsonantien leben die Blindtexte";
         int skip[ALPHABETGROESSE]; //Buchstaben von 1-26; 0. Stelle fuer Leerzeichen reserviert
+        char suchText[MAX];
 
-        int ergebnis = mischarsearch(suchText, text, skip);
-    if(ergebnis == -1){
-        printf("Das Wort wurde nicht gefunden!");
-    }else{
-        printf("Das Wort wurde ab %d. Stelle im zu suchenden Text gefunden!", ergebnis);
-    }
+        printf("Menü\nGeben Sie an, was Sie machen wollen:\n\n");
+        int eingabe = 1;
+        int ergebnis = -1;
+        while(eingabe != 5){
+            printf("1) konstanten Text ausgeben\n2) Suchwort eingeben und prüfen[mit Boyer- Moore]\n3) Suchwort eingeben und prüfen[mit Rabin- Karp]\n4) Suchwort im Text anzeigen lassen\n5) Programm beenden\n\n");
+            scanf("%d",&eingabe);
+            switch(eingabe){
+                case 1: printf("%s\n\n",text); break;
+                case 2: printf("Geben sie ein zu suchendes Wort ein:\n");
+                        scanf("%s",suchText);
+                        ergebnis = ergebnisMischarsearch(text, suchText, skip); break;
+                case 4: ergebnisAusgabe(text, suchText, ergebnis);
+                default: break;
+            }
+        }
         return 0;
     }
